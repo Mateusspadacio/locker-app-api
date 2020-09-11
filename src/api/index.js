4e845cf7e47fa2eef9f6ad372c1515c42b2f59de
Mateusspadacio <mateusspadacio@gmail.com>
2020-09-10 22:49:54 -0300

    new locker route

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { userRouter } from './resources/user/user.router';
+import { lockerRouter } from './resources/locker/locker.router';
 
 export const restRouter = express.Router();
 restRouter.use('/users', userRouter);
+restRouter.use('/locker', lockerRouter);
